8b9f1027c4d52ce2a78a410b56fef76f33b30b01
jonathan <weyermannx@gmail.com>
2019-10-10 14:27:42 -0600

    First hopefully functioning version

--- pdf2img.js
+++ pdf2img.js
@@ -1,44 +1,43 @@
 'use strict';
 
-var fs      = require('fs');
-var path    = require('path');
-var pdf2img = require('pdf2img-lambda-friendly');
+const fs = require('fs');
+const path = require('path');
+const pdf2img = require('pdf2img-lambda-friendly');
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ region: 'us-west-2' });
 require('events').EventEmitter.defaultMaxListeners = 100;
 
-const aws = require('aws-sdk');
-
-const s3 = new aws.S3({ region: 'us-west-2' });
-
-//var input   = __dirname + '/test.pdf';
 
 pdf2img.setOptions({
   type: 'png',                                // png or jpg, default jpg
-  density: 200,                               // default 600
+  density: 150,                               // default 600
   outputdir: __dirname + path.sep + 'output', // output folder, default null (if null given, then it will create folder name same as file name)
   outputname: 'test',                         // output file name, dafault null (if null given, then it will create image name same as input name)
 });
 
 const grab_file_from_s3 = user => {
   return new Promise((resolve, reject) => {
-    s3.getObject({
+    const destPath = `/tmp/${user}.pdf`
+    var params = {
       Bucket: process.env.BUCKET,
       Key: `pdfs/${user}.pdf`
-    }, (err, data) => {
-      if ( err ) reject(err)
-           else resolve(data)
-    });
+    }
+    const s3Stream = s3.getObject(params).createReadStream();
+    const fileStream = fs.createWriteStream(destPath);
+    s3Stream.on('error', reject);
+    fileStream.on('error', reject);
+    fileStream.on('close', () => { resolve(destPath);});
+    s3Stream.pipe(fileStream);
   });
 };
 
-
-
-function save_file_to_s3(path, name) {
-  console.log(`path: ${path}`)
+const save_file_to_s3 = (path, name, user) => {
+  console.log(`path: ${JSON.stringify(path)}`)
   fs.readFile(path, (err, data) => {
     var d = new Date();
     s3.putObject({
       Bucket: process.env.BUCKET,
-      Key: `test/${name}`,
+      Key: `${user}/${name}`,
       ContentType: data.type,
       Body: data
     }).promise()
@@ -53,35 +52,40 @@ function save_file_to_s3(path, name) {
 };
 
 module.exports.execute = async (event, context, callback) => {
-  console.log(`hello: ${event['user']}`);
-  const response = "";
+  event['Records'].forEach(async (record) => {
+    console.log(record);
+    const key = record.s3.object.key;
+    console.log(key);
+    const split = key.split('/')
+    console.log(split);
+    const user = split[split.length-1].split('.')[0]
+    console.log(`New .pdf object has been created: ${user})`);
 
-  async () => {
     try {
-      response = await grab_file_from_s3(event['user'])
-    } catch (err) {
-      console.error(err)
-    }
-  }
-  console.log(`response: ${JSON.stringify(response)}`)
+        var response = await grab_file_from_s3(user)
 
-  pdf2img.convert(response, function(err, info) {
-    console.log(response);
-    if (err) console.log(err)
-    else {
-      console.log(`info: ${JSON.stringify(info)}`);
-      for (var i = 0; i < info["message"].length; i++) {
-        console.log(info["message"][i]["path"]);
-        save_file_to_s3(info["message"][i]["path"], info["message"][i]["name"]);
-      }
+        pdf2img.convert(response, function(err, info) {
+          console.log(response);
+          if (err) console.log(err)
+          else {
+            console.log(`info: ${JSON.stringify(info)}`);
+            for (var i = 0; i < info["message"].length; i++) {
+              console.log(`imp: ${info["message"][i]["path"]}`);
+              save_file_to_s3(info["message"][i]["path"], info["message"][i]["name"], user);
+            }
+          }
+        });
+    } catch (err) {
+        console.error(err)
     }
+
   });
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Your function executed successfully!',
         input: event,
       },
       null,
