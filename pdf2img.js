59a19addde7070dd346bc69f7c64e7fefc03b0f7
jonathan <weyermannx@gmail.com>
2019-10-17 00:00:33 -0600

    removed comments, greatly reduced the number of lines of code

--- pdf2img.js
+++ pdf2img.js
@@ -3,32 +3,26 @@
 const fs = require('fs');
 const path = require('path');
 const AWS = require('aws-sdk');
-
 const exec = require('await-exec')
 require('events').EventEmitter.defaultMaxListeners = 100;
 
 const grabPdfFromS3 = user => {
   const S3 = new AWS.S3({ region: 'us-west-2' });
-  console.log(`user: ${user}`)
   return new Promise((resolve, reject) => {
     const destPath = `/tmp/${user}.pdf`
     var params = {
       Bucket: process.env.BUCKET,
       Key: `pdfs/${user}.pdf`
     }
-    console.log(`params: ${JSON.stringify(params)}`)
     S3.headObject(params)
       .promise()
       .then(() => {
         const s3Stream = S3.getObject(params).createReadStream()
-        console.log(`s3Stream: ${JSON.stringify(s3Stream)}`)
         const fileStream = fs.createWriteStream(destPath);
-        console.log(`fileStream: ${JSON.stringify(fileStream)}`)
         s3Stream.on('error', reject);
         fileStream.on('error', reject);
         fileStream.on('close', () => { resolve(destPath);});
         s3Stream.pipe(fileStream);
-        console.log(`fileStream: ${JSON.stringify(s3Stream.pipe(fileStream))}`)
       })
       .catch(error => {
         reject(error)
@@ -56,67 +50,34 @@ const saveFileToS3 = (path,key) => {
   });
 };
 
-module.exports.execute = async (event, context, callback) => {
-  //return new Promise((resolve, reject) => {
-  var record = event['Records'][0]
-  //event['Records'].forEach(async (record) => {
-  console.log(record);
-  const key = record.s3.object.key;
-  console.log(key);
-  const split = key.split('/')
-  console.log(split);
-  const user = split[split.length-1].split('.')[0]
-  console.log(`New .pdf object has been created: ${user}`);
+const currentUrl = (index) => {
+  return `/tmp/image${index}.jpg`
+}
+const s3Url = (user, index) => {
+  return `${user}/image${index}.jpg`
+}
 
+const user = (event) => {
+  const key = event['Records'][0].s3.object.key;
+  const split = key.split('/')
+  return split[split.length-1].split('.')[0]
+}
+
+module.exports.execute = async (event, context, callback) => {
   try {
-      var path = await grabPdfFromS3(user)
-      console.log(`path: ${path}`);
-      console.log('./lambda-ghostscript/bin/gs -sDEVICE=jpeg -dTextAlphaBits=4 -r128 -o /tmp/image%d.jpg ' + path);
-      await exec('./lambda-ghostscript/bin/gs -sDEVICE=jpeg -dTextAlphaBits=4 -r128 -o /tmp/image%d.jpg ' + path);
+      var local_pdf_path = await grabPdfFromS3(user(event))
+      await exec('./lambda-ghostscript/bin/gs -sDEVICE=jpeg -dTextAlphaBits=4 -r128 -o /tmp/image%d.jpg ' + local_pdf_path);
 
       var index = 1
-      var currentUrl = `/tmp/image${index}.jpg`
-      var s3Url = `${user}/image${index}.jpg`
-      console.log(`currentUrl: ${currentUrl}`)
-      while (fs.existsSync(currentUrl)) {
-        //file exists
-        var response = await saveFileToS3(currentUrl, s3Url);
+      while (fs.existsSync(currentUrl(index))) {
+        var response = await saveFileToS3(currentUrl(index), s3Url(user(event), index));
         index++;
-        console.log(`response: ${response}`)
-        console.log(`s3Url: ${s3Url}`)
-        console.log(`currentUrl: ${currentUrl}`)
-        currentUrl = `/tmp/image${index}.jpg`
-        s3Url = `${user}/image${index}.jpg`
       }
-      console.log(`index: ${index}`);
       if (index > 1) {
-        console.log("success, I guess")
-        //callback(null, 200)
-        return {
-          statusCode: 200,
-          body: JSON.stringify(
-            {
-              message: 'Your function executed successfully!',
-              input: `${index} images were uploaded`,
-            },
-            null,
-            2
-          ),
-        }
+        return { statusCode: 200, body: JSON.stringify({message: 'Success', input: `${index} images were uploaded`}, null, 2)}
       }
   } catch (err) {
-      console.log(err)
-      return {
-        statusCode: 500,
-        body: JSON.stringify(
-          {
-            message: err,
-            input: event,
-          },
-          null,
-          2
-        ),
-      }
+    return { statusCode: 500, body: JSON.stringify({message: err,input: event}, null, 2)}
   }
 };
 
